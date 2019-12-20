7c42a15c59f6205eeb3efe80674b3be97498b9fc
storyxc <andresiniesta@foxmail.com>
2019-12-20 18:47:51 +0800

    fix

--- src/page/editor.vue
+++ src/page/editor.vue
@@ -58,7 +58,7 @@ export default {
   },
   methods: {
     publish() {
-      this.$axios.get('http://localhost:8080/story/user')
+      this.$axios.get('/story/user')
         .then(res => {
           console.log(res);
         })
